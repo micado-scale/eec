6444922074d9ef1be65ca8f53dc6f66d3cb8df6a
jaydesl <jay.deslauriers@gmail.com>
2020-12-18 18:23:37 +0000

    Add tests

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -1,4 +1,9 @@
+import base64
 import pytest
+import json
+import io
+
+from werkzeug.datastructures import FileStorage
 
 from micado_eec.micado import app
 
@@ -8,6 +13,24 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+
+def b64_yaml():
+    adt = b"""\
+    tosca_definitions_version: tosca_simple_yaml_1_2
+    imports:
+    - micado_types.yaml
+    inputs:
+      test:
+        description: test adt input
+    topology_template:
+      node_templates:
+        stressng:
+          type: tosca.nodes.MiCADO.Container.Application.Docker.Deployment
+          properties:
+            image: lorel/docker-stress-ng
+    """
+    return base64.b64encode(adt).decode("utf-8")
 
 
 def test_health_response(client):
@@ -20,19 +43,30 @@ def test_protocols_response(client):
     assert rv.json["protocols"] == []
 
 
-def test_get_ports_user_adt(client):
-    body = {"artefact_data": {}}
+def test_get_ports_with_json_artefact(client):
+    body = {"artefact_data": {"downloadUrl_content": b64_yaml()}}
     rv = client.get("micado_eec/get_ports", json=body)
-    for element in rv.json["free_inputs"]:
-        if "adt.yaml" in element.values():
-            break
-    else:
-        pytest.fail("ADT missing from free inputs")
+    assert rv.status_code == 200
 
 
-def test_get_ports_preregistered_adt(client):
-    body = {"artefact_data": {"adt": "mybase64adtgoeshere"}}
+def test_get_ports_with_file_artefact(client):
+    body = {"downloadUrl_content": b64_yaml()}
+    print(body)
+    my_file = FileStorage(
+        stream=io.BytesIO(json.dumps(body).encode("utf-8")),
+        filename="artefact_data",
+    )
+    rv = client.get(
+        "micado_eec/get_ports",
+        data={"artefact_data": my_file},
+        content_type="multipart/form-data",
+    )
+    assert rv.status_code == 200
+
+
+def test_get_ports_parameters(client):
+    body = {"artefact_data": {"downloadUrl_content": b64_yaml()}}
     rv = client.get("micado_eec/get_ports", json=body)
-    for element in rv.json["free_inputs"]:
-        if "adt.yaml" in element.values():
-            pytest.fail("ADT present in free inputs")
+    assert len(rv.json["parameters"]) == 1
+    assert rv.json["parameters"][0]["key"] == "test"
+    assert rv.json["parameters"][0]["description"] == "test adt input"
